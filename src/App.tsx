b2fc1f6d7d613679747772301bd284ce76ec6b81
Jakub Madyda <jakub.madyda17@gmail.com>
2023-04-27 18:54:45 +0200

    prettier setup and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 
 function App() {
+    const [task, setTask] = useState('');
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <form action="">
+                <label htmlFor="task">Task</label>
+                <input
+                    id="task"
+                    type="text"
+                    value={task}
+                    onChange={e => setTask(e.target.value)}
+                />
+                <button>Add</button>
+            </form>
+        </>
+    );
 }
 
-export default App
+export default App;
