bfc8ac76175f5861d7d80245f9e62a013532921a
Jakub Madyda <jakub.madyda17@gmail.com>
2023-04-27 19:00:14 +0200

    add inputs and textarea for add task form

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { useState } from 'react';
 
 function App() {
     const [task, setTask] = useState('');
+    const [description, setDescription] = useState('');
 
     return (
         <>
-            <form action="">
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                }}
+            >
                 <label htmlFor="task">Task</label>
                 <input
                     id="task"
@@ -13,7 +18,13 @@ function App() {
                     value={task}
                     onChange={e => setTask(e.target.value)}
                 />
-                <button>Add</button>
+                <label htmlFor="description">Description</label>
+                <textarea
+                    id="description"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                ></textarea>
+                <button type="submit">Add</button>
             </form>
         </>
     );
