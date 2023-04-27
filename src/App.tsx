c434821fdfe45885d92a95a14081df778cbd64e3
Jakub Madyda <jakub.madyda17@gmail.com>
2023-04-27 19:56:00 +0200

    sets adding tasks to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,72 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+interface Task {
+    name: string;
+    description: string;
+    addedDate: Date;
+    status: 'open' | 'closed';
+    id: number;
+}
 
 function App() {
-    const [task, setTask] = useState('');
+    const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+        getTasksApi('tasks').then(setTasks);
+    }, []);
+
+    async function getTasksApi(endpoint: string): Promise<Task[]> {
+        const response = await axios.get<Task[]>(
+            `http://localhost:3000/${endpoint}`
+        );
+        return response.data;
+    }
+
+    async function sendTaskApi(
+        endpoint: string,
+        data: Omit<Task, 'id'>
+    ): Promise<Task> {
+        try {
+            const response = await axios.post(
+                `http://localhost:3000/api/v1/${endpoint}`,
+                data
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async function handleSubmit() {
+        const data = await sendTaskApi('tasks', {
+            addedDate: new Date(),
+            description,
+            name,
+            status: 'open',
+        });
+        setTasks([...tasks, data]);
+        setName('');
+        setDescription('');
+    }
 
     return (
         <>
             <form
-                onSubmit={e => {
+                onSubmit={async e => {
                     e.preventDefault();
+                    await handleSubmit();
                 }}
             >
                 <label htmlFor="task">Task</label>
                 <input
                     id="task"
                     type="text"
-                    value={task}
-                    onChange={e => setTask(e.target.value)}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
                 <label htmlFor="description">Description</label>
                 <textarea
@@ -26,6 +76,16 @@ function App() {
                 ></textarea>
                 <button type="submit">Add</button>
             </form>
+            <div>
+                {tasks.map(task => (
+                    <div key={task.id}>
+                        <b>{task.name} </b>
+                        <span>{task.description} </span>
+                        <button>Add operation</button>
+                        <button>Finish</button>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
